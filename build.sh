958312929693765f44a7c20d0e11f7322ad6d4e0
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-11 15:03:06 +0100

    build.sh file for deployment command

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+python manage.py collectstatic
+
+python manage.py migrate
+
+# Create superuser
+# python manage.py createsu
+
+# Initialize settings
+# python manage.py createsettings
+
+# Generate data
+# python manage.py generate_prod_data
