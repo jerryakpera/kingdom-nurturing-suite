e03f65960f95bf5472284787a87a2fd240727048
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-12 07:15:14 +0100

    Fix static files handling

--- project/settings/base.py
+++ project/settings/base.py
@@ -123,11 +123,11 @@ STORAGES = {
     },
 }
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "kns", "static"),
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
