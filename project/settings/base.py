cb6a3c827a155f9798d36a41186ab897f560e78e
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-13 05:36:35 +0100

    Templates configurations

--- project/settings/base.py
+++ project/settings/base.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR, "kns", "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
