1974ce89490f8e43ac32fa452c2eab3a04ec764f
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-13 12:03:13 +0100

    Reset password

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", include("admin_honeypot.urls")),
     path("control-panel/", admin.site.urls),
     path("accounts/", include("kns.accounts.urls")),
+    # Password reset urls
+    path(
+        "reset_password/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/pages/authentication/reset_password.html"
+        ),
+        name="reset_password",
+    ),
+    path(
+        "reset_password_sent/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/pages/authentication/reset_password_sent.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/pages/authentication/reset.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_password_complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/pages/authentication/reset_password_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
 
 admin.site.site_title = "KNS Admin Portal"
