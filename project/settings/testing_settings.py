936a97d4168113d2dd33337be1ecb9c5efc276ad
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-13 13:56:59 +0100

    Set files for coverage to omit

--- project/settings/testing_settings.py
+++ project/settings/testing_settings.py
@@ -1,16 +1,5 @@
-import dj_database_url
-from decouple import config
-
 from .base import *  # noqa
-from .base import BASE_DIR
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-DATABASES["default"] = dj_database_url.parse(config("TEST_DATABASE_URL"))
