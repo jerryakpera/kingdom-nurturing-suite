3128e43961e5c20f1ab042b1875daa27debc940b
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-19 16:39:37 +0100

    Add group modal

---
+++ kns/static/groups/js/group_modal.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const modalButtons = document.querySelectorAll(
+    '[id^="open-group-modal-button-"]'
+  );
+
+  modalButtons.forEach((button) => {
+    const groupId = button.id.split('-').pop();
+    const modalId = `extralarge-modal-${groupId}`;
+    const closeButtonId = `close-group-modal-button-${groupId}`;
+
+    const $targetEl = document.getElementById(modalId);
+
+    const options = {
+      placement: 'bottom-right',
+      backdrop: 'dynamic',
+      backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+      closable: true,
+    };
+
+    const modal = new Modal($targetEl, options);
+
+    button.addEventListener('click', () => {
+      modal.show();
+    });
+
+    document.getElementById(closeButtonId).addEventListener('click', () => {
+      modal.hide();
+    });
+  });
+});
