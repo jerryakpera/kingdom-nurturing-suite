176591542b87b6edf40ac471574e192776528205
Jeremiah Akpera <jerryakpera@gmail.com>
2024-08-23 14:45:47 +0100

    Add user banners

--- kns/custom_user/admin.py
+++ kns/custom_user/admin.py
@@ -7,4 +7,24 @@ from django_use_email_as_username.admin import BaseUserAdmin
 
 from .models import User
 
-admin.site.register(User, BaseUserAdmin)
+
+class CustomUserAdmin(BaseUserAdmin):
+    """
+    Custom admin configuration for the User model.
+    """
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            "Additional Info",
+            {
+                "fields": ("verified", "is_visitor", "agreed_to_terms"),
+            },
+        ),
+    )
+
+    # If you want to include these fields in the list display in the admin panel
+    # list_display = BaseUserAdmin.list_display + ('verified', 'is_visitor', 'agreed_to_terms')
+
+
+# Register the User model with the custom admin class
+admin.site.register(User, CustomUserAdmin)
--- tailwind.config.js
+++ tailwind.config.js
@@ -62,6 +62,13 @@ module.exports = {
         },
       },
     },
+    screens: {
+      xs: '576px',
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+      xl: '1440px',
+    },
   },
   plugins: [],
 };
