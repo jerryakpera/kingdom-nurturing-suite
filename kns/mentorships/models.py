c364e29f42bde1912a41e745838c495803e499b8
Jeremiah Akpera <jerryakpera@gmail.com>
2024-09-12 14:29:33 +0100

    MentorshipGoal model with tests

--- kns/mentorships/models.py
+++ kns/mentorships/models.py
@@ -9,13 +9,9 @@ from django.db import models
 from django.utils import timezone
 from tinymce import models as tinymce_models
 
+from kns.core.constants import MENTORSHIP_GOAL_TYPES, MENTORSHIP_STATUS_CHOICES
 from kns.core.modelmixins import ModelWithStatus, TimestampedModel
 from kns.profiles.models import Profile
-
-# from core.suite.constants import (
-#     MENTORSHIP_GOAL_TYPES,
-#     MENTORSHIP_STATUS_CHOICES,
-# )
 
 
 class MentorshipArea(
@@ -69,6 +65,64 @@ class MentorshipArea(
         -------
         str
             The title of the mentorship area.
+        """
+
+        return self.title
+
+
+class MentorshipGoal(
+    TimestampedModel,
+    ModelWithStatus,
+    models.Model,
+):
+    """
+    A model representing a mentorship goal.
+
+    The `MentorshipGoal` model stores information about specific goals for
+    mentorship. It includes fields for the title, slug, content, the author
+    (profile) who created the goal, and the type of the goal. The model also
+    inherits timestamp fields for tracking creation and modification times,
+    and a status field for indicating the current state of the mentorship goal.
+
+    Methods
+    -------
+    __str__():
+        Return the title of the mentorship goal, providing a string
+        representation of the object.
+    """
+
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(
+        unique=True,
+        default=uuid4,
+        null=True,
+        blank=True,
+        editable=False,
+    )
+    content = tinymce_models.HTMLField()
+    author = models.ForeignKey(
+        Profile,
+        related_name="mentorship_goalss_created",
+        on_delete=models.CASCADE,
+    )
+    type = models.CharField(
+        max_length=30,
+        choices=MENTORSHIP_GOAL_TYPES,
+        default="primary",
+    )
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the MentorshipGoal instance.
+
+        This method returns the title of the mentorship goal, which is used
+        to represent the object in a readable form, such as in Django admin
+        or when the object is printed.
+
+        Returns
+        -------
+        str
+            The title of the mentorship goal.
         """
 
         return self.title
--- kns/mentorships/tests/test_models.py
+++ kns/mentorships/tests/test_models.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from kns.custom_user.models import User
-from kns.mentorships.models import MentorshipArea
+from kns.mentorships.models import MentorshipArea, MentorshipGoal
 
 
 class TestMentorshipAreaModel(TestCase):
@@ -67,3 +67,72 @@ class TestMentorshipAreaModel(TestCase):
         )
 
         self.assertEqual(str(mentorship_area), "Software Engineering")
+
+
+class TestMentorshipGoalModel(TestCase):
+    def setUp(self):
+        """
+        Setup method to create a Profile instance linked to the test user.
+        """
+        self.user = User.objects.create_user(
+            email="jane.doe@example.com",
+            password="password",
+        )
+
+        self.profile = self.user.profile
+        self.profile.first_name = "Jane"
+        self.profile.last_name = "Doe"
+
+        self.profile.save()
+
+    def test_mentorship_goal_creation(self):
+        """
+        Test that a MentorshipGoal instance can be created and is properly saved.
+        """
+        mentorship_goal = MentorshipGoal.objects.create(
+            title="Career Development",
+            content="<p>Goals for career growth and advancement.</p>",
+            author=self.profile,
+            type="primary",
+        )
+
+        self.assertEqual(mentorship_goal.title, "Career Development")
+        self.assertEqual(
+            mentorship_goal.content,
+            "<p>Goals for career growth and advancement.</p>",
+        )
+        self.assertEqual(mentorship_goal.type, "primary")
+
+    def test_mentorship_goal_slug_unique(self):
+        """
+        Test that the slug field on the MentorshipGoal model is unique.
+        """
+        MentorshipGoal.objects.create(
+            title="Career Development",
+            content="<p>Goals for career growth and advancement.</p>",
+            author=self.profile,
+            type="primary",
+        )
+
+        with pytest.raises(IntegrityError) as excinfo:
+            MentorshipGoal.objects.create(
+                title="Career Development",
+                content="<p>Different content.</p>",
+                author=self.profile,
+                type="secondary",
+            )
+
+        assert "UNIQUE constraint failed" in str(excinfo.value)
+
+    def test_mentorship_goal_str_method(self):
+        """
+        Test that the __str__ method returns the title of the mentorship goal.
+        """
+        mentorship_goal = MentorshipGoal.objects.create(
+            title="Career Development",
+            content="<p>Goals for career growth and advancement.</p>",
+            author=self.profile,
+            type="primary",
+        )
+
+        self.assertEqual(str(mentorship_goal), "Career Development")
